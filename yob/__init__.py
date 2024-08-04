2a9669b5d19188af3abdedbd8ba41a8823bb9e49
yansong <yansongc@gmail.com>
2024-08-05 11:45:56 +1200

    update and init upload folders

--- yob/__init__.py
+++ yob/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_hashing import Hashing
+import os
 
-from yob.config import DEFAULT_PASSWORD_SALT, DEFAULT_SECRET_KEY
+from yob.config import DEFAULT_PASSWORD_SALT, DEFAULT_SECRET_KEY,DEFAULT_PROFILE_IMAGES_FOLDER,DEFAULT_COMPETITOR_IMAGES_FOLDER,DEFAULT_COMPETITION_IMAGES_FOLDER
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -21,3 +22,14 @@ from yob import update_password
 from yob import profile
 from yob import profile_image
 from yob import register
+
+# Create image upload directories and save to app
+def init_upload_folder(key, value):
+    folder = os.path.join(os.path.dirname(__file__), value)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    app.config[key] = folder
+
+init_upload_folder('DEFAULT_PROFILE_IMAGES_FOLDER', DEFAULT_PROFILE_IMAGES_FOLDER)
+init_upload_folder('DEFAULT_COMPETITOR_IMAGES_FOLDER',DEFAULT_COMPETITOR_IMAGES_FOLDER)
+init_upload_folder('DEFAULT_COMPETITION_IMAGES_FOLDER',DEFAULT_COMPETITOR_IMAGES_FOLDER)
