10022094ce4b7787c04ee459b13e109e1ca9d467
yansong <yansongc@gmail.com>
2024-08-07 11:00:35 +1200

    fix circle imports and reuse get_user_by_id

--- yob/__init__.py
+++ yob/__init__.py
@@ -1,46 +1,41 @@
 from flask import Flask, g
-from flask_hashing import Hashing
 import os
 
 import yob.config as config
-from yob.config import DEFAULT_PROFILE_IMAGES_FOLDER,DEFAULT_COMPETITOR_IMAGES_FOLDER,DEFAULT_COMPETITION_IMAGES_FOLDER
+from yob.config import DEFAULT_PROFILE_IMAGES_FOLDER, DEFAULT_COMPETITOR_IMAGES_FOLDER, DEFAULT_COMPETITION_IMAGES_FOLDER
 from yob.login_manage import LoginManager
-from .database import Cursor
+from .repositories.users_repository import get_user_by_id
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # create an instance of hashing
-app.hashing = Hashing(app)
+# app.hashing = Hashing(app)
 
 # Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = config.DEFAULT_SECRET_KEY
 
-from . import  decorators, views
+from . import decorators, views
 
 # Create image upload directories and save to app
+
 def init_upload_folder(key, value):
     folder = os.path.join(app.root_path, value)
     if not os.path.exists(folder):
         os.makedirs(folder)
     app.config[key] = folder
 
+
 init_upload_folder('PROFILE_IMAGES_ABS_PATH', DEFAULT_PROFILE_IMAGES_FOLDER)
-init_upload_folder('COMPETITOR_IMAGES_ABS_PATH',DEFAULT_COMPETITOR_IMAGES_FOLDER)
-init_upload_folder('COMPETITION_IMAGES_ABS_PATH',DEFAULT_COMPETITION_IMAGES_FOLDER)
+init_upload_folder('COMPETITOR_IMAGES_ABS_PATH', DEFAULT_COMPETITOR_IMAGES_FOLDER)
+init_upload_folder('COMPETITION_IMAGES_ABS_PATH', DEFAULT_COMPETITION_IMAGES_FOLDER)
 
 # initial of LoginManager
 login_manager = LoginManager(app, login_view='login')
 
+
 @login_manager.user_loader
 def load_user(user_id):
-    """user loader for login manager"""
-    with Cursor(dictionary=True) as cursor:
-        cursor.execute(
-            'SELECT user_id, username, first_name, last_name, location,email,description, profile_image, '
-            'role, status, created_at FROM users WHERE user_id = %s',
-            (user_id,))
-        user = cursor.fetchone()
-    return user
+    return get_user_by_id(user_id)
 
 
 @app.context_processor
