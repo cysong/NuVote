bf8279719e4ee132b244b567765acc249de03332
yansong <yansongc@gmail.com>
2024-08-06 13:50:12 +1200

    unify validation regex

--- yob/config.py
+++ yob/config.py
@@ -13,4 +13,7 @@ DEFAULT_PROFILE_IMAGE = '/static/images/default_profile.png'
 DEFAULT_COMPETITOR_IMAGES_FOLDER = 'static/uploads/competitor_images'
 DEFAULT_COMPETITION_IMAGES_FOLDER = 'static/uploads/competition_images'
 DEFAULT_MIN_BIRTHDATE = '1900-01-01'
+USERNAME_REGX = r'^[a-zA-Z0-9_-]{3,50}$'
 DEFAULT_PASSWORD_REGEX = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+EMAIL_REGEX = r'^([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+$'
+ALLOWED_EXTENSIONS = ('png', 'jpg', 'jpeg', 'gif', '.webp', '.bmp')
