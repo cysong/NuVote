07c0b98595b6897a44b200c5681b8e39441c34dc
Hao Chen <hao.chen@lincolnuni.ac.nz>
2024-08-08 21:07:57 +1200

    added Announcements Mgmt

--- yob/views/announcements_mgmt.py
+++ yob/views/announcements_mgmt.py
@@ -1,1 +1,13 @@
 # ANNOUNCEMENTS
+from flask import render_template
+
+from yob import app
+from yob.decorators import login_required, admin_required
+
+
+@app.route('/announcements')
+@login_required
+@admin_required
+def announcements_mgmt():
+    # Render the admin home page with user information
+    return render_template('announcements/announcements_mgmt.html')
