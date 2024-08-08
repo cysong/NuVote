8e5f594b82b9b8b39215683db8be578feb746993
yansong <yansongc@gmail.com>
2024-08-09 10:31:23 +1200

    add competition relative views and templates

---
+++ yob/views/competitions.py
@@ -0,0 +1,18 @@
+# ENGAGING WITH COMPETITIONS
+from flask import render_template, g, request, jsonify, abort, redirect, url_for, flash
+from yob import app
+
+
+@app.route('/competition/manage')
+def competition_manage():
+    return render_template('competitions/competion_mgmt.html')
+
+
+@app.route('/competition/edit/<int:competition_id>')
+def competition_edit(competition_id):
+    return render_template('competitions/competion_edit.html')
+
+
+@app.route('/competition/view/<int:competition_id>')
+def competition_view(competition_id):
+    return render_template('competitions/competion_view.html')
