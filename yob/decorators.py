bcab24f59f20f83caac61e82d111b094b4782f8d
Tong <163929652+Tong-Ye-1159668@users.noreply.github.com>
2024-08-12 10:35:21 +1200

    Updated docstring in decorators.py

--- yob/decorators.py
+++ yob/decorators.py
@@ -7,39 +7,47 @@ from yob import app
 
 # Helper functions to manage session data
 def is_logged_in():
+    '''Check if the user is logged in.'''
     return ('loggedin' in session and session['loggedin'] and
             'user_id' in session and 'username' in session and 'role' in session)
 
 
 def get_role_from_session():
+    '''Get the role of the user from the session.'''
     return session['role'] if 'role' in session else ''
 
 
 def get_status_from_session():
+    '''Get the status of the user from the session.'''
     return session['status'] if 'status' in session else ''
 
 
 def get_user_id_from_session():
+    '''Get the user ID from the session.'''
     return session['user_id'] if 'user_id' in session else ''
 
 
 def get_username_id_from_session():
+    '''Get the username from the session.'''
     return session['username'] if 'username' in session else ''
 
 
 def get_profile_image_from_session():
+    '''Get the profile image from the session.'''
     return session['profile_image'] if 'profile_image' in session else ''
 
 
 # Context processor to inject user information into templates
 @app.context_processor
 def inject_user():
+    '''Inject user information into templates.'''
     return dict(is_logged_in=is_logged_in(), role=get_role_from_session(), user_status=get_status_from_session(),
                 user_id=get_user_id_from_session(), username=get_username_id_from_session(),
                 profile_image=get_profile_image_from_session(), request=request)
 
 
 def login_required(f):
+    '''Decorator to check if the user is logged in.'''
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not is_logged_in():
@@ -51,6 +59,7 @@ def login_required(f):
 
 
 def admin_required(f):
+    '''Decorator to check if the user is an administrator.'''
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'role' not in session or session['role'] != 'admin':
@@ -62,6 +71,7 @@ def admin_required(f):
 
 
 def owner_required(f):
+    '''Decorator to check if the user is the owner of the resource.'''
     @wraps(f)
     def decorated_function(*args, **kwargs):
         user_id = kwargs.get('user_id')
@@ -74,6 +84,7 @@ def owner_required(f):
 
 
 def inactive_user_message(f):
+    '''Decorator to display a message if the user is inactive.'''
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'status' in session and session['status'] == 'inactive':
@@ -85,6 +96,7 @@ def inactive_user_message(f):
 
 
 def inactive_user_action_check(f):
+    '''Decorator to check if the user is inactive and restrict certain actions.'''
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if request.method == 'POST' and 'status' in session and session['status'] == 'inactive':
