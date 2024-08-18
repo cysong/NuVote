5c7051784113ae1f9c2387f42a78ec71ce79602a
Tong <163929652+Tong-Ye-1159668@users.noreply.github.com>
2024-08-18 20:03:45 +1200

    changed comment to docstrings

--- yob/repositories/profile_image_repository.py
+++ yob/repositories/profile_image_repository.py
@@ -3,7 +3,7 @@ from yob.database import Cursor
 
 
 def handle_profile_image_update(user_id, profile_path=DEFAULT_PROFILE_IMAGE):
-    # Store the img_url in the database
+    '''Store the img_url in the database'''
     with Cursor() as cursor:
         cursor.execute("UPDATE users SET profile_image = %s WHERE user_id = %s",
                        (f"{profile_path}", user_id))
