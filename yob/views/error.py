b0560e0a231b36863d28a36158a8a4096d3a6747
Tong <163929652+Tong-Ye-1159668@users.noreply.github.com>
2024-08-18 15:33:26 +1200

    Changed comments to docstrings

--- yob/views/error.py
+++ yob/views/error.py
@@ -7,30 +7,31 @@ from yob import app
 
 @app.errorhandler(400)
 def bad_request(e):
-    # Render custom 400 error page
+    '''Render custom 400 error page'''
     return render_template('error/400.html'), 400
 
 
 @app.errorhandler(403)
 def bad_request(e):
-    # Render custom 403 error page
+    '''Render custom 403 error page'''
     return render_template('error/403.html'), 403
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # Render custom 404 error page
+    '''Render custom 404 error page'''
     return render_template('error/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    # Render custom 500 error page
+    '''Render custom 500 error page'''
     return render_template('error/500.html'), 500
 
 
 @app.errorhandler(Exception)
 def handle_exception(e):
+    '''Handle all exceptions'''
     logging.exception(e)
     message = str(e) or 'An unexpected error occurred.'
     return render_template('error/500.html', message=message), 500
