4d540894947c7bae8cc33b1974e91b2e22126b7e
yansong <yansongc@gmail.com>
2024-08-20 23:59:07 +1200

    add testcases for admin

--- tests/e2e/cypress/constants.js
+++ tests/e2e/cypress/constants.js
@@ -1,4 +1,10 @@
+import path from 'path';
+import { randomId } from './utils';
 
 export const DEFAULT_PASSWORD = 'Vote@2024'
+export const VOTER_USERNAME = randomId("voter")
+export const SCRUTINEER_USERNAME = randomId("scrutineer")
 export const ADMIN_USERNAME = 'admin'
 export const COMPETITION_ID_TO_VOTE = 4
+
+export const COMPETITION_NAME = randomId("Bird of the year ");
--- tests/e2e/cypress/utils.js
+++ tests/e2e/cypress/utils.js
@@ -3,3 +3,16 @@ export function randomId(prefix) {
     const randomPart = Math.floor(10000 + Math.random() * 90000); // 5 digits
     return `${prefix}${randomPart}`;
 }
+
+export function getDateAfterDays(days) {
+    const today = new Date();
+    
+    // get date after days
+    const targetDate = new Date(today);
+    targetDate.setDate(today.getDate() + days);
+    
+    // format date "YYYY-MM-DDTHH:mm"
+    const formattedDate = targetDate.toISOString().slice(0, 16);
+    
+    return formattedDate;
+  }
